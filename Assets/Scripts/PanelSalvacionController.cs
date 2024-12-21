38d11a2dcb526e6dc70a09986d27105d63bc6f1d
MegalyZ <73939964+MegalyZ@users.noreply.github.com>
2024-12-21 17:20:21 +0100

    CambioSpriteSalvarAlien

--- Assets/Scripts/PanelSalvacionController.cs
+++ Assets/Scripts/PanelSalvacionController.cs
@@ -9,14 +9,17 @@ public class PanelSalvacionController : MonoBehaviour
     public static int aliens, maxAliens;
     public static int numeroTotalAliens;
 
+    public GameObject Alien1;
+    public GameObject Alien2;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         numeroTotalAliens = GameObject.FindGameObjectsWithTag("alien").Length;
-        Debug.Log("numero aliens: " +  numeroTotalAliens);
+        Debug.Log("numero aliens: " + numeroTotalAliens);
 
         maxAliens = PlayerPrefs.GetInt("maxScore", 0);
-        maxScore.text = "Max: " + maxAliens.ToString();        
+        maxScore.text = "Max: " + maxAliens.ToString();
 
         aliens = 0;
         score.text = "Aliens saved: 0/" + numeroTotalAliens.ToString();
@@ -25,17 +28,16 @@ public class PanelSalvacionController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "alien")
         {
-            
+
             aliens++;
             score.text = "Aliens saved: " + aliens.ToString() + "/" + numeroTotalAliens.ToString();
-
 
             if (aliens > maxAliens)
             {
@@ -46,12 +48,29 @@ public class PanelSalvacionController : MonoBehaviour
 
             Destroy(collision.gameObject);
 
-            if(aliens == numeroTotalAliens) 
+            if (aliens == numeroTotalAliens)
             {
                 Debug.Log("todos los aliens salvados");
                 SceneManager.LoadScene("Win");
             }
 
+            Invoke("CambioDeSprite1", 0.01f);
+            Invoke("CambioDeSprite2", 1f);
+
         }
+    }
+
+    private void CambioDeSprite1()
+    {
+        Alien1.SetActive(false);
+        Alien2.SetActive(true);
+
+    }
+
+    private void CambioDeSprite2()
+    {
+
+        Alien1.SetActive(true);
+        Alien2.SetActive(false);
     }
 }
--- Assets/Scripts/VisualTutorial.cs
+++ Assets/Scripts/VisualTutorial.cs
@@ -14,6 +14,7 @@ public class VisualTutorial : MonoBehaviour
     void InstanciadorCanvas()
     {
         gameObject.SetActive(true);
+       
     }
 
     void DesInstanciadorCanvas()
